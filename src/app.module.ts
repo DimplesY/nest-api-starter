5a66c56f57ec4dc2237ede3fab29d0ea6dcd2999
Dimples_YJ <2890841438@qq.com>
2025-05-12 15:53:40 +0800

    feat(context): 添加 nestjs-cls 模块支持
    为项目引入基于 cls 的上下文管理模块，提供请求级别的共享存储能力

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '@/logger/logger.module';
 import { ConfigModule } from '@nestjs/config';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { ClsModule } from 'nestjs-cls';
 
 @Module({
   imports: [
+    ClsModule.forRoot({
+      middleware: {
+        mount: true,
+      },
+    }),
     DrizzleModule,
     ConfigModule.forRoot({
       isGlobal: true,
